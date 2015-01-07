65976174bb649dbdf26d6d0c50795671e117dfc7
hkmshb <hkmshb@gmail.com>
2015-01-07 09:46:22 +0100

    Enhanced code readability and renamed some functions
    * Added space around assignment and comparison statements
    * Adjusted functions to be two-line apart as per PEP8

--- Fourkilled.py
+++ Fourkilled.py
@@ -6,7 +6,7 @@ exactly as they are in value and position (1st, 2nd, 3rd and 4th) from the left.
 Each number in the players guess is taken and compared with these four unique 
 numbers and then a result is returned. The player keeps guessing until he gets 
 these numbers exactly as they are in value and in position. The results are 
-returned in terms of killed(k),injured(i) and none(n).
+returned in terms of killed(k), injured(i) and none(n).
 
 A number in the player's guess is said to be killed if that number is equivalent
 to that in the unique numbers in value and in position; Also a number is said to 
@@ -44,7 +44,7 @@ sells=[0,0,0,0]
 
 
 
-def CollectInput():
+def collect_input():
     """collect the input of the user"""
     while True:
         y = raw_input("Guess Number (4 digits): ").strip()
@@ -63,120 +63,114 @@ def CollectInput():
     return cells
 
 
-
-def killed(guess,values):
-    count=0
+def get_killed_count(guess, values):
+    count = 0
     for gues in range(len(guess)):
         for val in range(len(values)):
-            if gues==val and guess[gues]==values[val]:
+            if gues == val and guess[gues] == values[val]:
                 count+=1
     if count == 0:
         return ''
     return str(count)+'k'
 
 
-
-def Injured(guess,values):
-    count=0
+def get_injured_count(guess,values):
+    count = 0
     for val in range(len(values)):
         for gu in range(len(guess)):
-            if gu!=val and guess[gu]==values[val]:
-                count+=1
-    if count==0:
+            if gu != val and guess[gu] == values[val]:
+                count += 1
+    if count == 0:
         return ''            
     return str(count)+'i'
 
 
-
-def Nonne(guess,values):
-    count=0
+def get_none_count(guess,values):
+    count = 0
     for val in range(len(values)):
         for gues in range(len(guess)):
-            if guess[gues]!=values[val]:
+            if guess[gues] != values[val]:
                 count+=1
             if count == 16:
                 return str(4)+'n'
-    if count!=16 and count%4==0:
+    if count != 16 and (count % 4) == 0:
         return ''
     return str(count%4)+'n'
 
-
             
 def is_unique(cells):
-    """Ensures the numbers are unique i.e none of the numbers are repeated"""
+    """Ensures the numbers are unique i.e get_none_count of the numbers are 
+       repeated
+    """
     for column in range(len(cells)):
         for number in range(len(cells)):
-            if number!= column and cells[number]==cells[column]:
+            if number != column and cells[number] == cells[column]:
                 return False
     return True
-
 
 
 def is_valid(cells):
     """Ensures the numbers are valid"""
     for number in range(len(cells)):
-        if cells[number] <1 or cells[number] >9 or not is_unique(cells):
+        if cells[number] < 1 or cells[number] > 9 or not is_unique(cells):
             return False
     return True
 
 
-
 def fill(cells):
     for i in range(len(cells)):
-        cells[i]=random.randint(1,9)
+        cells[i] = random.randint(1,9)
     return cells
-
 
 
 def default(cells):
     for i in range(len(cells)):
-        cells[i]=0
+        cells[i] = 0
     return cells
 
 
+def result(guess, values):
+    return "%s%s%s" % (
+        get_killed_count(guess, values),
+        get_injured_count(guess, values),
+        get_none_count(guess, values)
+    )
 
-def result(guess,values):
-    r=str(killed(guess,values)+Injured(guess,values)+Nonne(guess,values))
-    return r
-        
-        
-        
-        
+
 def app():
-    Unique_Values=fill(sells)
+    Unique_Values = fill(sells)
     while not is_valid(Unique_Values):
-        Unique_Values= fill(sells)
+        Unique_Values = fill(sells)
         
-    Players_Guess=CollectInput()
+    Players_Guess = collect_input()
     while Players_Guess != Unique_Values:
         if is_valid(Players_Guess):
             print(">>> %s" % result(Players_Guess,Unique_Values))
             #print Unique_Values #(to see if the results are correct)
             print ''
-            Players_Guess=CollectInput()
+            Players_Guess = collect_input()
         else:
             print(">>> Error: An integer number of 4 unique digits not "
                   "containing zero expected.\n")
-            Players_Guess=CollectInput()
+            Players_Guess = collect_input()
     print(">>> *! Hurray \o/ !* %s\n" % result(Players_Guess,Unique_Values))
 
-           
-            
-            
+
 def main():
     print __doc__
     
     confirm_msg = "Press any key to continue or Q to quit..."
     x= 'Y'
-    while x !='N':
+    while x != 'N':
         app()
-        x=raw_input(confirm_msg)
+        x = raw_input(confirm_msg)
         try:
-            x=x.upper()
+            x = x.upper()
         except AttributeError:
-            x=raw_input(confirm_msg)
+            x = raw_input(confirm_msg)
 
     sys.exit()
+
 
 if __name__ == '__main__':
     main()
